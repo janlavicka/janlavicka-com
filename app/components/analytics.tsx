c8fa2d170e037a56882b3e0e47ee52af6274a221
Jan Lavička <iam@janlavicka.com>
2022-06-25 07:27:40 +0000

    chore: update dependencies

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -11,35 +11,33 @@ export default function Analytics() {
   }, [location]);
 
   const renderGtag = () => {
-    if (data.env.GA_UA_ID && data.env.GA_V4_ID) {
-      return (
-        <>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${data.env.GA_UA_ID}`}
-          />
-          <script
-            async
-            id="gtag-script"
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${data.env.GA_UA_ID}', {
-                  page_path: window.location.pathname,
-                });
-                gtag('config', '${data.env.GA_V4_ID}', {
-                  page_path: window.location.pathname,
-                });
-              `,
-            }}
-          />
-        </>
-      );
-    } else {
-      return <></>;
-    }
+    if (!data.env.GA_UA_ID || !data.env.GA_V4_ID) return <></>;
+
+    return (
+      <>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${data.env.GA_UA_ID}`}
+        />
+        <script
+          async
+          id="gtag-script"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${data.env.GA_UA_ID}', {
+                page_path: window.location.pathname,
+              });
+              gtag('config', '${data.env.GA_V4_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </>
+    );
   };
 
   return <>{renderGtag()}</>;
