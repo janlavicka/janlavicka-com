54d55a41802bff8ea6d9b96eca28de8ef1156320
Jan Lavička <info@janlavicka.com>
2025-12-04 19:52:30 +0100

    chore: update dependencies

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,2 @@
 /// <reference types="vite/client" />
+/// <reference path=".react-router/types/+future.ts" />
--- workers/app.ts
+++ workers/app.ts
@@ -1,20 +1,20 @@
-import { createRequestHandler } from "react-router";
+import { createContext, createRequestHandler, RouterContextProvider } from "react-router";
 
-declare module "react-router" {
-  export interface AppLoadContext {
-    cloudflare: {
-      env: Env;
-      ctx: ExecutionContext;
-    };
-  }
+interface CloudflareContext {
+  env: Env;
+  ctx: ExecutionContext;
 }
+
+export const cloudflareContext = createContext<CloudflareContext>();
 
 const requestHandler = createRequestHandler(() => import("virtual:react-router/server-build"), import.meta.env.MODE);
 
 export default {
   async fetch(request, env, ctx) {
-    return requestHandler(request, {
-      cloudflare: { env, ctx },
-    });
+    const context = new RouterContextProvider();
+
+    context.set(cloudflareContext, { env, ctx });
+
+    return requestHandler(request, context);
   },
 } satisfies ExportedHandler<Env>;
