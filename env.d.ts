3925bedfe5ae1203b7df633dbc5c3fd6e91356ed
Jan Lavička <info@janlavicka.com>
2026-02-06 12:27:26 +0100

    fix: type error

--- env.d.ts
+++ env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="vite/client" />
 /// <reference path=".react-router/types/+future.ts" />
+
+import type { ClapCounterDurableObject } from "@/durable-objects/ClapCounterDurableObject";
+
+declare global {
+  interface Env {
+    CLAP_COUNTER: DurableObjectNamespace<ClapCounterDurableObject>;
+  }
+}
